11fde65b9330e55a3280956e6b606541ec1fb063
ayan-hassan <ahassan@hotmail.ca>
2023-02-15 22:20:29 -0500

    completed and edited function
    Co-authored-by: Ahmed Alhajahmed <Alhajahmed@users.noreply.github.com>

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/* For this challenge, you'll be implementing a word search solver, as a function that
+receives a 2D array of letters and a word. The function must:
 
-module.exports = wordSearch
+    * Check to find the word horizontally and vertically
+    * Return true if the word is found, and return false if the word is not found */
+
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i]).join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
